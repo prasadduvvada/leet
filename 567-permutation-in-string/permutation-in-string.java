fcc8259182ba56d5986662a1c458c06b02f483e8
prasadduvvada <129745272+prasadduvvada@users.noreply.github.com>
2026-01-19 21:45:29 +0530

    Time: 6 ms (86.06%) | Memory: 44.2 MB (56.38%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -2,33 +2,21 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int n1 = s1.length();
         int n2 = s2.length();
-        
+        int s1_arr [] = new int[26];
+        int s2_arr [] = new int[26];
         if (n1 > n2) return false;
-
-        int[] cnt1 = new int[26];
-        int[] cnt2 = new int[26];
-
-        for (int i = 0; i < n1; i++) {
-            cnt1[s1.charAt(i) - 'a']++;
-            cnt2[s2.charAt(i) - 'a']++;
+        for(char x: s1.toCharArray()) {
+           s1_arr[x-'a']++;
         }
-
-        if (isMatch(cnt1, cnt2)) return true;
-
-        for (int i = n1; i < n2; i++) {
-            cnt2[s2.charAt(i) - 'a']++;
-            cnt2[s2.charAt(i - n1) - 'a']--;
-
-            if (isMatch(cnt1, cnt2)) return true;
+        for(int i=0; i<n2 ; i++){
+            s2_arr[s2.charAt(i)-'a']++;
+            if(i>=n1){
+                s2_arr[s2.charAt(i-n1)-'a']--;
+            }
+            if(Arrays.equals(s1_arr,s2_arr)){
+                return true;
+            }
         }
-
-        return false;
-    }
-
-    private boolean isMatch(int[] a, int[] b) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) return false;
-        }
-        return true;
+    return false;
     }
 }
