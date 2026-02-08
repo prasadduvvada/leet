3dfc4d41f07284b663bb05e98baef0bf6e762bb7
prasadduvvada <129745272+prasadduvvada@users.noreply.github.com>
2026-02-08 18:11:32 +0530

    Time: 16 ms (91.08%) | Memory: 45.7 MB (26.39%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -4,7 +4,6 @@ class Solution {
         int closet = nums[0]+nums[1]+nums[2];
         int n = nums.length;
         for(int i=0; i<n-2; i++){
-            if(i > 0 && nums[i] == nums[i-1]) continue;
             int cur = nums[i];
             int left = i+1;
             int right = n-1;
