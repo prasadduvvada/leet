a88302d463a1dcc6e30920940f31293305cdd6b3
prasadduvvada <129745272+prasadduvvada@users.noreply.github.com>
2026-02-20 05:36:22 +0530

    Time: 13 ms (22.51%) | Memory: 47.2 MB (16.97%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,26 +1,23 @@
 class Solution {
     public int bestClosingTime(String customers) {
-        int n = customers.length();
-        int maxScore = 0;
-        int currentScore = 0;
-        int bestHour = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (customers.charAt(i) == 'Y') {
-                // 'Y' means being open was good, so "score" improves
-                currentScore++;
-            } else {
-                // 'N' means being open was bad, so "score" drops
-                currentScore--;
-            }
-
-            // If we found a better score, this is the new best hour to close
-            if (currentScore > maxScore) {
-                maxScore = currentScore;
-                bestHour = i + 1;
-            }
+      int n = customers.length();
+      int prefixn[] = new int[n+1];
+      int suffixy[] = new int[n+1];
+      for(int i=0; i<n; i++){
+        prefixn[i+1] = prefixn[i]+(customers.charAt(i) == 'N' ? 1:0);
+      }
+      for(int i = n-1; i>=0; i--){
+         suffixy[i] = suffixy[i+1]+(customers.charAt(i) == 'Y' ? 1:0);
+      }
+      int min = Integer.MAX_VALUE;
+      int best =0;
+      for(int i=0; i<=n; i++){
+        int cur = prefixn[i] + suffixy[i];
+        if(cur < min){
+            min = cur;
+            best =i;
         }
-
-        return bestHour;
+      }
+      return best;
     }
 }
