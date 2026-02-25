ff46ac0bbada8b9e0a321a81e5188792436ff6a8
prasadduvvada <129745272+prasadduvvada@users.noreply.github.com>
2026-02-25 21:55:46 +0530

    Time: 2 ms (94.43%) | Memory: 72.1 MB (42.53%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,15 +1,27 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int prefix[] = new int[nums.length];
-        prefix[0] = 1;
-        for(int i=1; i<nums.length; i++){
-           prefix[i] = prefix[i-1] * nums[i-1];
+        int n = nums.length;
+        int res[] = new int[n]; // This will store our final answer
+
+        // STEP 1: FILL WITH PREFIX PRODUCTS
+        // res[i] will store the product of everything to the LEFT of index i
+        res[0] = 1; // Nothing is to the left of the first element
+        for(int i = 1; i < n; i++){
+           // Current prefix = (Previous prefix) * (The number we just passed)
+           res[i] = res[i - 1] * nums[i - 1];
         }
-        int suffix =1;
-        for(int i = nums.length-1; i>=0; i--){
-            prefix[i] = prefix[i] * suffix;
-            suffix *=nums[i];
+
+        // STEP 2: MULTIPLY BY SUFFIX PRODUCTS
+        // We travel backwards, keeping track of the product of everything to the RIGHT
+        int suffix = 1; // Nothing is to the right of the last element
+        for(int i = n - 1; i >= 0; i--){
+            // Final Answer = (Everything on Left) * (Everything on Right)
+            res[i] = res[i] * suffix;
+            
+            // Update suffix for the next element to the left
+            suffix *= nums[i];
         }
-        return prefix;
+
+        return res;
     }
 }
